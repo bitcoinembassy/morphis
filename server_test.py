a66052d658aed271a5a2657036ca81e7bd76e6b8
samm <samm@satoshi.hq.codingninjas.org>
2014-12-28 21:56:23 -0500

    Further fix server_test.

--- server_test.py
+++ server_test.py
@@ -11,12 +11,15 @@ print("Listening on %s." % listen_address)
 
 while True:
     # Wait for next request from client.
-    message = ssocket.recv()
+    address, empty, message = ssocket.recv_multipart()
+
+#    message = ssocket.recv()
+
     print("Received request: %s" % message)
 
     # simulate work
     time.sleep(1);
 
-    ssocket.send(b"World")
+    ssocket.send_multipart([address, b'', b"World"])
     print("Sent response!")
 
