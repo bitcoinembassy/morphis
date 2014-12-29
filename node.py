e06a16cace6f7359504e809b0be73b9b2d8b9d11
samm <samm@morph.is>
2014-12-29 04:59:15 -0500

    Working p2p poller implementation.
    Used REQ<<->ROUTER, not sure if such is optimal, as now we have to
    register with the poller each additional REQ as it is created. Using a
    ROUTER<->ROUTER setup didn't work due to a flaw with ROUTER requiring
    the server (thing being connect()ed to) to set an identifier. Looks like
    the server then could forge the identifier causing conflicts and such.

--- node.py
+++ node.py
@@ -1,59 +1,91 @@
+import threading
 import asyncio
 import time
 import zmq
 
-def engageNet(loop, pipe):
+from zhelpers import zpipe
+
+def engageNet(loop, context, pipe):
     listen_address = "tcp://*:5555"
 
-    context = zmq.Context()
     ssocket = context.socket(zmq.ROUTER)
+    ssocket.identity = b"asdf";
     ssocket.bind(listen_address)
 
     print("S: Listening on {}.".format(listen_address))
 
-    csocket = context.socket(zmq.REQ)
+    csocket = context.socket(zmq.ROUTER)
 
     poller = zmq.Poller()
+    poller.register(pipe, zmq.POLLIN)
     poller.register(ssocket, zmq.POLLIN)
-    poller.register(csocket. zmq.POLLIN)
-    poller.register(pipe, zmq.
+#    poller.register(csocket, zmq.POLLIN)
 
     i = 0
+    clientid = 0
+
+    csocks = {}
 
     while True:
-        socks = dict(poller.poll())
+        try:
+            ready_socks = poller.poll()
+        except:
+            break
 
-        if ssocket in socks:
-            # Wait for next request from client.
-            address, empty, message = ssocket.recv_multipart()
+        print("WOKEN")
 
-            print("S: Received request [{}] from [{}].".format(message, address))
+        for sockt in ready_socks:
+            sock = sockt[0]
+            csockid = csocks.get(sock.fd)
 
-            ssocket.send_multipart([address, b'', b"World"])
-            print("S: Sent response #{} to [{}]!".format(i, address))
-            i = i + 1
+            if csockid != None:
+                message = sock.recv()
 
-        if csocket in socks:
-            message = csocket.recv()
-            print("C: Received response: [{}].".format(message))
-            
+                print("C: Received response [{}] from [{}].".format(message, csockid))
 
-def client(loop):
-    connect_address = "tcp://localhost:5555"
+                csocket.send(b"Hello")
+                print("C: Sent request to [{}]!".format(csockid))
+            elif sock == ssocket:
+                # Wait for next request from client.
+                address, empty, message = ssocket.recv_multipart()
 
-            csocket.send(b"Hello")
-            print("C: Sent request!")
+                print("S: Received request [{}] from [{}].".format(message, address))
 
+                ssocket.send_multipart([address, b'', b"World"])
+                print("S: Sent response #{} to [{}]!".format(i, address))
+                i = i + 1
+            elif sock == pipe:
+                message = pipe.recv_multipart()
+                print("XS: Received [{}] message.".format(message[0]))
 
-    socket.connect(connect_address)
-    print("Connecting to {}.".format(connect_address))
+                cmd = message.pop(0)
+                if cmd == b"conn":
+                    addr = message.pop(0)
+                    #addr = message[1];
+                    print("C: Connecting to [{}].".format(addr))
 
-    while True:
+                    csocket = context.socket(zmq.REQ)
+                    csocket.connect(addr)
+
+                    csocks[csocket.fd] = clientid
+                    clientid += 1
+                    poller.register(csocket, zmq.POLLIN)
+
+                    csocket.send(b"Hello")
+                    print("C: Sent request!")
 
 def main():
+    context = zmq.Context()
+
+    pipe = zpipe(context)
+
     loop = asyncio.get_event_loop()
 
-    loop.run_in_executor(None, server, loop)
-    loop.run_in_executor(None, client, loop)
+    #loop.run_in_executor(None, engageNet, loop, context, pipe[0])
+    thread = threading.Thread(target=engageNet, args=(loop, context, pipe[1]))
+#    thread.daemon = True
+    thread.start()
+
+    pipe[0].send_multipart([b"conn", b"tcp://localhost:5555"])
 
 main()
