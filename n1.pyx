8a83e8dad3afa85093ad84dd693681a24752707b
samm <samm@morph.is>
2015-01-01 14:20:01 -0500

    Update n1.pyx to handle new enc.pyx changes.

--- n1.pyx
+++ n1.pyx
@@ -6,11 +6,12 @@ def debug(str message):
 def main():
     debug("Entered main().")
 
-    keys = enc.generate_RSA(4096)
+    private_key = enc.generate_RSA(4096)
+    public_key = private_key.publickey();
     
-    debug("Private Key=[%s], Public Key=[%s]." % (str(keys[0]),  str(keys[1])))
+    debug("Private Key=[%s], Public Key=[%s]." % (str(private_key.exportKey("PEM")),  str(public_key.exportKey("PEM"))))
 
-    id = enc.generate_ID(keys[1])
+    id = enc.generate_ID(public_key.exportKey("DER"))
 
     debug("id=[%s]." % id.hexdigest())
 
