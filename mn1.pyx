ef3de06e263be2af2d0d06cde8a462004ebb74db
samm <samm@morph.is>
2015-01-02 03:30:47 -0500

    Add start of mn1.pyx, so far is working Hello echo server.
    Using Dave Behnke's example
    (http://davebehnke.com/python-asyncio-streams-client-server.html) to get
    started.

---
+++ mn1.pyx
@@ -0,0 +1,52 @@
+import asyncio
+import llog
+import logging
+
+logger = None
+clients = {} # task, [reader, writer]
+
+def log():
+    global logger
+    if logger == None:
+        logger = logging.getLogger(__name__)
+    return logger
+
+def accept_client(client_reader, client_writer):
+    global log
+
+    task = asyncio.Task(handle_client(client_reader, client_writer))
+    clients[task] = (client_reader, client_writer)
+
+    def client_done(task):
+        del clients[task]
+        client_writer.close()
+        log().info("End connection.")
+
+    log().info("New connection.")
+    task.add_done_callback(client_done)
+
+@asyncio.coroutine
+def handle_client(client_reader, client_writer):
+    client_writer.write("HELLO\n".encode())
+
+def main():
+    global log
+
+    print("Starting server.")
+    log().info("Starting server.")
+    loop = asyncio.get_event_loop()
+    f = asyncio.start_server(accept_client, host=None, port=5555)
+    loop.run_until_complete(f)
+    loop.run_forever()
+
+if __name__ == "__main__":
+    #log.setLevel(logging.DEBUG)
+    #formatter = logging.Formatter("%(asctime)s %(levelname)s [%(module)s:%(lineno)d] %(message)s")
+    #ch = logging.StreamHandler()
+    #ch.setLevel(logging.DEBUG)
+    #ch.setFormatter(formatter)
+    #log.addHandler(ch)
+
+    llog.init()
+
+    main()
