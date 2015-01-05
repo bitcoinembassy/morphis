b4dcfed4e718998b32a835f7c49a70c04bd606aa
samm <samm@morph.is>
2015-01-05 04:40:39 -0500

    Add more test protocol, track client failures.

--- mn1.pyx
+++ mn1.pyx
@@ -3,7 +3,8 @@ import llog
 import logging
 
 logger = None
-clients = {} # task, [reader, writer]
+clientPipes = {} # task, [reader, writer]
+clientObjs = {} # remoteAddress, dict
 
 def log():
     global logger
@@ -12,13 +13,21 @@ def log():
     return logger
 
 def accept_client(client_reader, client_writer):
-    global log
+    global log, clientObjs
 
-    task = asyncio.Task(handle_client(client_reader, client_writer))
-    clients[task] = (client_reader, client_writer)
+    peer_name = str(client_writer.get_extra_info("peername")[0])
+    log().info("peer_name [{}].".format(peer_name))
+    client = clientObjs.get(peer_name)
+    if client == None:
+        log().info("Initializing new clientObj.")
+        client = {}
+        clientObjs[peer_name] = client
+
+    task = asyncio.Task(handle_client(client, client_reader, client_writer))
+    clientPipes[task] = (client_reader, client_writer)
 
     def client_done(task):
-        del clients[task]
+        del clientPipes[task]
         client_writer.close()
         log().info("End connection.")
 
@@ -26,13 +35,26 @@ def accept_client(client_reader, client_writer):
     task.add_done_callback(client_done)
 
 @asyncio.coroutine
-def handle_client(client_reader, client_writer):
+def handle_client(client, client_reader, client_writer):
+    fails = client.get("fails")
+    if fails != None and fails >= 5:
+        client_writer.transport().abort()
+        return
+
     client_writer.write("SSH-2.0-mNet_0.0.1\n".encode())
+
+    log().info("peer=[{}].".format(client))
 
     try:
         data = yield from asyncio.wait_for(client_reader.readline(), timeout=2.0)
     except asyncio.TimeoutError:
         log().warning("Client sent no line data by timeout.")
+        fails = client.get("fails")
+        if fails == None:
+            fails = 1
+        else:
+            fails += 1
+        client["fails"] = fails
         assert data == None
         return
 
