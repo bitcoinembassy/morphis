c970ef8870737215766797dfa7ead85d28f885e0
samm <samm@morph.is>
2015-01-06 02:16:19 -0500

    Fix logging framework to be able to use just log, not log().

--- llog.pyx
+++ llog.pyx
@@ -7,8 +7,14 @@ def init():
     global logging_initialized
 #    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s [%(module)s:%(lineno)d] %(message)s")
 
-    if (not logging_initialized):
-#        print("Reading in logging.ini.")
-        logging.config.fileConfig("logging.ini")
-        logger = logging.getLogger(__name__)
-#        logger.info("Logger initialized.")
+    if (logging_initialized):
+        return
+
+#    print("Reading in logging.ini.")
+    logging.config.fileConfig("logging.ini")
+    logger = logging.getLogger(__name__)
+#    logger.info("Logger initialized.")
+    logging_initialized = True
+
+if not logging_initialized:
+    init()
--- mn1.pyx
+++ mn1.pyx
@@ -2,24 +2,19 @@ import asyncio
 import llog
 import logging
 
-logger = None
 clientPipes = {} # task, [reader, writer]
 clientObjs = {} # remoteAddress, dict
 
-def log():
-    global logger
-    if logger == None:
-        logger = logging.getLogger(__name__)
-    return logger
+log = logging.getLogger(__name__)
 
 def accept_client(client_reader, client_writer):
     global log, clientObjs
 
     peer_name = str(client_writer.get_extra_info("peername")[0])
-    log().info("peer_name [{}].".format(peer_name))
+    log.info("peer_name [{}].".format(peer_name))
     client = clientObjs.get(peer_name)
     if client == None:
-        log().info("Initializing new clientObj.")
+        log.info("Initializing new clientObj.")
         client = {}
         clientObjs[peer_name] = client
 
@@ -29,9 +24,9 @@ def accept_client(client_reader, client_writer):
     def client_done(task):
         del clientPipes[task]
         client_writer.close()
-        log().info("End connection.")
+        log.info("End connection.")
 
-    log().info("New connection.")
+    log.info("New connection.")
     task.add_done_callback(client_done)
 
 @asyncio.coroutine
@@ -43,12 +38,12 @@ def handle_client(client, client_reader, client_writer):
 
     client_writer.write("SSH-2.0-mNet_0.0.1\n".encode())
 
-    log().info("peer=[{}].".format(client))
+    log.info("peer=[{}].".format(client))
 
     try:
         data = yield from asyncio.wait_for(client_reader.readline(), timeout=2.0)
     except asyncio.TimeoutError:
-        log().warning("Client sent no line data by timeout.")
+        log.warning("Client sent no line data by timeout.")
         fails = client.get("fails")
         if fails == None:
             fails = 1
@@ -59,13 +54,13 @@ def handle_client(client, client_reader, client_writer):
         return
 
     sdata = data.decode().rstrip()
-    log().info("Received [{}].".format(sdata))
+    log.info("Received [{}].".format(sdata))
 
 def main():
     global log
 
     print("Starting server.")
-    log().info("Starting server.")
+    log.info("Starting server.")
     loop = asyncio.get_event_loop()
     f = asyncio.start_server(accept_client, host=None, port=5555)
     loop.run_until_complete(f)
