e23d2279a89e53e42f4743c2ad76344b07410e78
Sam Maloney <sam2015@morph.is>
2015-01-12 04:06:56 -0500

    Add exception handling function to llog.

--- llog.pyx
+++ llog.pyx
@@ -1,5 +1,7 @@
 import logging
 import logging.config
+import traceback
+import sys
 
 logging_initialized = False;
 
@@ -15,6 +17,10 @@ def init():
     logger = logging.getLogger(__name__)
 #    logger.info("Logger initialized.")
     logging_initialized = True
+
+def handle_exception(log, info):
+    log.fatal("{} threw [{}]: {}".format(info, sys.exc_info()[0], str(sys.exc_info()[0])))
+    traceback.print_tb(sys.exc_info()[2])
 
 if not logging_initialized:
     init()
