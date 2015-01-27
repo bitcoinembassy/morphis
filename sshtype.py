6f905c7a969307b5a7cce940501d1d248f6a08ed
Sam Maloney <sam2015@morph.is>
2015-01-26 23:39:33 -0500

    Add binary type to sshtype.

--- sshtype.py
+++ sshtype.py
@@ -10,18 +10,23 @@ def parseNameList(buf):
     return parseString(buf)
 
 def parseString(buf):
-    global log
-
     length = struct.unpack(">L", buf[0:4])[0]
     log.debug("length={}".format(length))
     value = buf[4:4 + length].decode()
 
-    return length, value
+    return length + 4, value
+
+def parseBinary(buf):
+    length = struct.unpack(">L", buf[0:4])[0]
+    log.debug("length={}".format(length))
+    value = buf[4:4 + length]
+
+    return length + 4, value
 
 def parseMpint(buf):
     length = struct.unpack(">L", buf[0:4])[0]
     log.debug("length={}".format(length))
-    return length, putil.inflate_long(buf[4:4+length])
+    return length + 4, putil.inflate_long(buf[4:4+length])
 
 def encodeMpint(val):
     buf = putil.deflate_long(val)
@@ -32,6 +37,15 @@ def encodeNameList(val):
     return encodeString(val)
 
 def encodeString(val):
+#    log.debug("type=[{}].".format(type(val)))
+#    if isinstance(val, bytes) or isinstance(val, bytearray):
+#        buf = val
+#    else:
     buf = val.encode(encoding="UTF-8")
+
+    length = struct.pack(">L", len(buf))
+    return length + buf
+
+def encodeBinary(buf):
     length = struct.pack(">L", len(buf))
     return length + buf
