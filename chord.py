4f547402034c5be31430c18c074e476822a9707a
Sam Maloney <sam2015@morph.is>
2015-02-23 19:42:59 -0500

    Start of some test peer connection management code.

--- chord.py
+++ chord.py
@@ -9,6 +9,7 @@ import rsakey
 import mn1
 import peer
 from mutil import hex_dump
+import random
 
 log = logging.getLogger(__name__)
 
@@ -19,6 +20,10 @@ class ChordEngine():
         self.server = None
         self.server_protocol = None
 
+        meta = {"connected": False}
+        self.peer_list = [] #[("127.0.0.{}".format(n), 5555, meta) for n in range(1, 100)]
+        self.unconnected_peer_cnt = len(self.peer_list)
+
         self.pending_connections = []
         self.peers = {} # {(host, port): Peer}.
 
@@ -28,12 +33,55 @@ class ChordEngine():
         self.node.get_loop().run_until_complete(self.server)
         log.info("Node listening on [{}:{}].".format(host, port))
 
+        self._process_connection_count()
+
     def stop(self):
         self.server.close()
+
+    def _process_connection_count(self):
+        desired = 1
+
+        while True:
+            cnt = len(self.pending_connections) + len(self.peers)
+            if cnt < desired:
+                if self._connect_peer():
+                    continue
+            break
+
+    def _connect_peer(self):
+        if self.unconnected_peer_cnt == 0:
+            log.info("Insufficient peers to maintain desired connection amount.")
+            return False
+
+        while True:
+            server = random.choice(self.peer_list)
+            # TODO: Optimize for when few servers available.
+            if not server[2]["connected"]:
+                break;
+
+        loop = self.node.get_loop()
+        client = loop.create_connection(self._create_client_protocol, server[0], server[1])
+
+        loop.run_until_complete(client)
+
+        self.unconnected_peer_cnt -= 1
+
+        return True
 
     def _create_server_protocol(self):
         ph = mn1.SshServerProtocol(self.node.get_loop())
         ph.set_server_key(self.node.get_node_key())
+
+        p = peer.Peer(self)
+        p.set_protocol_handler(ph)
+
+        self.pending_connections.append(p)
+
+        return ph
+
+    def _create_client_protocol(self):
+        ph = mn1.SshClientProtocol(self.node.get_loop())
+        ph.set_client_key(self.node.get_node_key())
 
         p = peer.Peer(self)
         p.set_protocol_handler(ph)
