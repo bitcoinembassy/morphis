2a01db032f1bb27e9b8909473bdf13eaea61e187
samm <samm@morph.is>
2015-03-13 20:54:59 -0400

    Have setup.py cythonize some files, ones that work.
    This makes a huge difference in performance still.

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
+modules = [\
+    "db",
+    "llog",
+    "bittrie",
+    "enc",
+    "mutil",
+    "putil",
+    "packet",
+    "sshtype",
+    "asymkey",
+    "rsakey"\
+]
+
 setup(
     name = 'n1',
-    ext_modules = cythonize("*.py"),
+    ext_modules = cythonize(\
+        [x + ".py" for x in modules])
 )
