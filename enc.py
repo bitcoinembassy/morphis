d4492751a5fc7f3adb812886f4af56d37a76d5aa
Sam Maloney <sam2015@morph.is>
2015-05-06 01:27:13 -0400

    Implement some minor cosmetic changes to enc.py code.

--- enc.py
+++ enc.py
@@ -1,10 +1,12 @@
+from Crypto.Hash import SHA512
+from Crypto.PublicKey import RSA
+
 def generate_RSA(bits=4096):
     '''
     Generate an RSA keypair with an exponent of 65537 in PEM format
     param: bits The key length in bits
     Return private key and public key
     '''
-    from Crypto.PublicKey import RSA
 
     new_key = RSA.generate(bits, e=65537)
     #public_key = new_key.publickey().exportKey("PEM")
@@ -13,8 +15,5 @@ def generate_RSA(bits=4096):
     #return private_key, public_key 
     return new_key;
 
-def generate_ID(public_key):
-    from Crypto.Hash import SHA512
-
-    return SHA512.new(public_key).digest()
-
+def generate_ID(data):
+    return SHA512.new(data).digest()
