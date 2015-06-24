4ce6ed7303bef195666c7e0e659253c9d4d43cb3
Sam Maloney <sam2015@morph.is>
2015-06-24 17:42:42 -0400

    Turn off --cleartexttransport on lots.sh launch script.

--- lots.sh
+++ lots.sh
@@ -5,7 +5,7 @@ rm -f morphis-*.log
 IDX=0;
 while :; do
     rm -f morphis.log;
-    python3 node.py -l logging-prod.ini --bind 127.0.0.1:6000 --addpeer 127.0.0.1:7002 --nn 2000 --nodecount 10 --dbpoolsize 10 --cleartexttransport --dumptasksonexit --parallellaunch --dburl postgresql://m1:2d8VhzVIovxZy@pg1/m1 --dm --instanceoffset ${IDX} &
+    python3 node.py -l logging-prod.ini --bind 127.0.0.1:6000 --addpeer 127.0.0.1:7002 --nn 2000 --nodecount 10 --dbpoolsize 10 --dumptasksonexit --parallellaunch --dburl postgresql://m1:2d8VhzVIovxZy@pg1/m1 --dm --instanceoffset ${IDX} &
     sleep 0.5
     mv morphis.log morphis-${IDX}.log
 
