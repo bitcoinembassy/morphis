d3ca532c1d7e55374f6c04a6acc8f33eb4256bf1
Sam Maloney <sam2015@morph.is>
2015-07-10 17:56:23 -0400

    Add parse_string_from(..) function to sshtype.py.

--- sshtype.py
+++ sshtype.py
@@ -9,6 +9,17 @@ log = logging.getLogger(__name__)
 def parseNameList(buf):
     return parseString(buf)
 
+def parse_string_from(buf, i):
+    length = struct.unpack_from(">L", buf, i)[0]
+
+    if log.isEnabledFor(logging.DEBUG):
+        log.debug("length={}".format(length))
+
+    start = i + 4
+    value = buf[start:start + length].decode()
+
+    return length + 4, value
+
 def parseString(buf):
     length = struct.unpack(">L", buf[0:4])[0]
     if log.isEnabledFor(logging.DEBUG):
