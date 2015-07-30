6ab2cf57aef420898572bc16a35f9fe845806278
Sam Maloney <sam2015@morph.is>
2015-07-30 12:07:19 -0400

    Fix llog config override to work when there are no other parameters.

--- llog.py
+++ llog.py
@@ -17,7 +17,7 @@ def init():
 
 #    print("Reading in logging.ini.")
     config_file = "logging.ini"
-    if len(sys.argv) > 3:
+    if len(sys.argv) >= 3:
         if sys.argv[1] == "-l":
             config_file =  sys.argv[2]
 
