58102f57e0442c7e7bd4374ba4c208972bba2389
Sam Maloney <sam2015@morph.is>
2015-07-30 21:08:41 -0400

    Fix mbase32 encoding scheme to have encoding in save order as value.
    Alphanumerically, as decided by unix sort as my standard.

--- mbase32.py
+++ mbase32.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2014-2015  Sam Maloney.
 # License: Public Domain.
 
-charset = "ybndrfg8ejkmcpqxot1uwisza345h769"
+charset = "13456789abcdefghijkmnopqrstuwxyz"
 
 def encode(val):
     assert type(val) in (bytes, bytearray), type(val)
