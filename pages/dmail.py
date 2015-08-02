f603e8dbd1f1f1b1c479ef093c6f361c66e84f51
Sam Maloney <sam2015@morph.is>
2015-08-02 04:36:05 -0400

    Fix Dmail compose, got broke if not passing To in url.

--- pages/dmail.py
+++ pages/dmail.py
@@ -122,7 +122,7 @@ def __serve_get(handler, rpath, done_event):
             if from_addr:
                 iframe_src = "../compose/form/{}".format(from_addr).encode()
             else:
-                iframe_src = "../compose/form"
+                iframe_src = "../compose/form".encode()
 
             content = [\
                 pages.dmail_compose_dmail_content[0].replace(\
