b566ab71fa734e1e772ca0927f8b082f01367862
Sam Maloney <sam2015@morph.is>
2015-08-08 14:35:09 -0400

    Fix node.py to default bind to ":4250"; "" as host is cross platform *.

--- node.py
+++ node.py
@@ -339,7 +339,7 @@ def __main():
     if bindaddr:
         bindaddr.split(':') # Just to preemptively test.
     else:
-        bindaddr = "*:4250"
+        bindaddr = ":4250"
     if args.cleartexttransport:
         log.info("Enabling cleartext transport.")
         mn1.enable_cleartext_transport()
