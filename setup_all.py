86f2cbeebe5c583b142e84df5ef664588e1317ec
Sam Maloney <samm@morph.is>
2016-06-14 16:09:07 -0400

    Fix setup_all.py (make test) to skip _nocython.py files.
    Now it is usable again as test compile stage to catch compile errors!

--- setup_all.py
+++ setup_all.py
@@ -1,10 +1,22 @@
 # Copyright (c) 2014-2015  Sam Maloney.
 # License: GPL v2.
 
+import os
+
 from distutils.core import setup
 from Cython.Build import cythonize
 
+files = []
+
+def add_path(files, path):
+    for entry in os.listdir(path):
+        if entry.endswith(".py") and not entry.endswith("_nocython.py"):
+            files.append(path + "/" + entry)
+
+add_path(files, os.getcwd())
+add_path(files, os.getcwd() + "/maalstroom/")
+
 setup(
     name = 'n1',
-    ext_modules = cythonize(["*.py", "maalstroom/*.py"])
+    ext_modules = cythonize(files)
 )
