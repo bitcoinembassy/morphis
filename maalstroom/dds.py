479ab06ffcd50e7b41c671f22ad764e100b932d1
Sam Maloney <samm@morph.is>
2016-07-01 13:23:28 -0400

    More cleanup of dds.py; rename methods to be consistent with url paths.

--- maalstroom/dds.py
+++ maalstroom/dds.py
@@ -48,20 +48,26 @@ def serve_get(dispatcher, rpath):
     elif req.startswith("/images/"):
         dispatcher.send_content(templates.dds_imgs[req[8:]])
         return
-    elif req == "/axon":
-        yield from _process_axon(dispatcher, req[5:])
+    elif req == "/axon/create":
+        # Render the Create Axon (Targeted or not) page.
+        yield from _process_axon_create(dispatcher, req[5:])
         return
     elif req.startswith("/axon/grok/"):
-        yield from _process_view_axon(dispatcher, req[11:])
+        # Render the Grok View; which shows the Axon, SynapseS and Synapse
+        # create form.
+        yield from _process_axon_grok(dispatcher, req[11:])
         return
     elif req.startswith("/axon/read/"):
-        yield from _process_read_axon(dispatcher, req[11:])
+        # Render an individual Axon.
+        yield from _process_axon_read(dispatcher, req[11:])
         return
     elif req.startswith("/axon/synapses/"):
+        # Scan for and render SynapseS connected to the requested Axon.
         yield from _process_axon_synapses(dispatcher, req[15:])
         return
     elif req.startswith("/synapse/create/"):
-        yield from _process_create_synapse(dispatcher, req[16:])
+        # Render the Create Synapse entry form.
+        yield from _process_synapse_create(dispatcher, req[16:])
         return
 
     dispatcher.send_error("request: {}".format(req), errcode=400)
@@ -75,70 +81,13 @@ def serve_post(dispatcher, rpath):
     req = rpath[s_dds_len:]
 
     if req == "/synapse/create":
-        yield from _process_create_axon_post(dispatcher, req)
+        yield from _process_synapse_create_post(dispatcher, req)
         return
 
     dispatcher.send_error("request: {}".format(req), errcode=400)
 
 @asyncio.coroutine
-def _process_create_axon_post(dispatcher, req):
-    dd = yield from dispatcher.read_post()
-    if not dd:
-        dispatcher.send_error("request: {}".format(req), errcode=400)
-        return
-
-    content = fia(dd["content"])
-    content2 = fia(dd.get("content2"))
-
-    if not content:
-        content = content2
-    elif content2:
-        content = content + "\r\n" + content2
-
-    if not content:
-        dispatcher.send_error("No content.", errcode=400)
-        return
-
-    key = None
-
-    def key_callback(akey):
-        nonlocal key
-        key = akey
-
-    yield from\
-        dispatcher.node.chord_engine.tasks.send_store_data(\
-            content.encode(), store_key=True, key_callback=key_callback)
-
-    target_addr = fia(dd["target_addr"])
-
-    if not target_addr:
-        resp =\
-            "Resulting&nbsp;<a href='morphis://.dds/axon/read/{axon_addr}'>"\
-                "Axon</a>&nbsp;Address:<br/>{axon_addr}"\
-                     .format(axon_addr=mbase32.encode(key))
-
-        dispatcher.send_content(resp)
-        return
-
-    target_addr = mbase32.decode(target_addr)
-
-    synapse = tb.Synapse.for_target(target_addr, key)
-
-    yield from\
-        dispatcher.node.chord_engine.tasks.send_store_synapse(\
-            synapse, store_key=True, key_callback=key_callback)
-
-    resp =\
-        "Resulting&nbsp;<a href='morphis://.dds/axon/read/{synapse_addr}/"\
-            "{target_addr}'>Synapse</a>&nbsp;Address:<br/>{synapse_addr}"\
-                 .format(\
-                    synapse_addr=mbase32.encode(key),\
-                    target_addr=mbase32.encode(target_addr))
-
-    dispatcher.send_content(resp)
-
-@asyncio.coroutine
-def _process_axon(dispatcher, req):
+def _process_axon_create(dispatcher, req):
     template = templates.dds_axon[0]
     template = template.format(\
         message_text="",
@@ -148,7 +97,7 @@ def _process_axon(dispatcher, req):
     dispatcher.send_content([template, req])
 
 @asyncio.coroutine
-def _process_view_axon(dispatcher, req):
+def _process_axon_grok(dispatcher, req):
     if req.startswith("@"):
         #TODO: Come up with a formal spec. We should probably deal with
         # unprintable characters by merging them, Etc.
@@ -183,6 +132,66 @@ def _process_view_axon(dispatcher, req):
 
     dispatcher.send_content(msg)
     return
+
+@asyncio.coroutine
+def _process_axon_read(dispatcher, req):
+    p0 = req.find('/')
+
+    if p0 > -1:
+        # Then the request is for a TargetedBlock.
+        key = mbase32.decode(req[:p0])
+        target_key = mbase32.decode(req[p0+1:])
+    else:
+        # Then the request is not for a TargetedBlock.
+        key = mbase32.decode(req)
+        target_key = None
+
+    axon = yield from _load_axon(dispatcher, key, target_key)
+
+    axon_id = axon.id if axon else None
+
+    if not axon or axon.data is None:
+        if target_key:
+            data_rw =\
+                yield from\
+                    dispatcher.node.chord_engine.tasks.send_get_targeted_data(\
+                        bytes(key), target_key=target_key)
+        else:
+            data_rw =\
+                yield from\
+                    dispatcher.node.chord_engine.tasks.send_get_data(\
+                        bytes(key))
+
+        data = data_rw.data
+    else:
+        data = axon.data
+
+    if not data:
+        dispatcher.send_content("Not found on the network at the moment.")
+        return
+
+    yield from _save_axon(dispatcher, key, target_key, data, axon_id)
+
+    if not data.startswith(MorphisBlock.UUID):
+        # Plain data, return it!
+        key_enc = mbase32.encode(key)
+
+        content = yield from _format_axon(dispatcher.node, data, key, key_enc)
+
+        template = templates.dds_synapse_view[0]
+
+        template = template.format(\
+            key=key_enc, content=content)
+
+        msg = "<head><link rel='stylesheet' href='morphis://.dds/style.css'></link></head><body style='height: 90%; padding:0;margin:0;'>{}</body>".format(template)
+
+        acharset = dispatcher.get_accept_charset()
+        dispatcher.send_content(\
+            msg, content_type="text/html; charset={}".format(acharset))
+        return
+
+    # We assume it is a Dmail if it is a MorphisBlock.
+    dispatcher.send_content(hex_dump(data))
 
 @asyncio.coroutine
 def _process_axon_synapses(dispatcher, axon_addr_enc):
@@ -262,64 +271,75 @@ def _process_axon_synapses(dispatcher, axon_addr_enc):
     dispatcher.end_partial_content()
 
 @asyncio.coroutine
-def _process_read_axon(dispatcher, req):
-    p0 = req.find('/')
-
-    if p0 > -1:
-        # Then the request is for a TargetedBlock.
-        key = mbase32.decode(req[:p0])
-        target_key = mbase32.decode(req[p0+1:])
-    else:
-        # Then the request is not for a TargetedBlock.
-        key = mbase32.decode(req)
-        target_key = None
-
-    axon = yield from _load_axon(dispatcher, key, target_key)
-
-    axon_id = axon.id if axon else None
-
-    if not axon or axon.data is None:
-        if target_key:
-            data_rw =\
-                yield from\
-                    dispatcher.node.chord_engine.tasks.send_get_targeted_data(\
-                        bytes(key), target_key=target_key)
-        else:
-            data_rw =\
-                yield from\
-                    dispatcher.node.chord_engine.tasks.send_get_data(\
-                        bytes(key))
-
-        data = data_rw.data
-    else:
-        data = axon.data
-
-    if not data:
-        dispatcher.send_content("Not found on the network at the moment.")
+def _process_synapse_create(dispatcher, target_addr):
+    if dispatcher.handle_cache(target_addr):
         return
 
-    yield from _save_axon(dispatcher, key, target_key, data, axon_id)
+    template = templates.dds_create_synapse[0]
 
-    if not data.startswith(MorphisBlock.UUID):
-        # Plain data, return it!
-        key_enc = mbase32.encode(key)
+    template = template.format(\
+        csrf_token=dispatcher.client_engine.csrf_token,\
+        message_text="",\
+        target_addr=target_addr)
 
-        content = yield from _format_axon(dispatcher.node, data, key, key_enc)
+    dispatcher.send_content(template)
 
-        template = templates.dds_synapse_view[0]
-
-        template = template.format(\
-            key=key_enc, content=content)
-
-        msg = "<head><link rel='stylesheet' href='morphis://.dds/style.css'></link></head><body style='height: 90%; padding:0;margin:0;'>{}</body>".format(template)
-
-        acharset = dispatcher.get_accept_charset()
-        dispatcher.send_content(\
-            msg, content_type="text/html; charset={}".format(acharset))
+@asyncio.coroutine
+def _process_synapse_create_post(dispatcher, req):
+    dd = yield from dispatcher.read_post()
+    if not dd:
+        dispatcher.send_error("request: {}".format(req), errcode=400)
         return
 
-    # We assume it is a Dmail if it is a MorphisBlock.
-    dispatcher.send_content(hex_dump(data))
+    content = fia(dd["content"])
+    content2 = fia(dd.get("content2"))
+
+    if not content:
+        content = content2
+    elif content2:
+        content = content + "\r\n" + content2
+
+    if not content:
+        dispatcher.send_error("No content.", errcode=400)
+        return
+
+    key = None
+
+    def key_callback(akey):
+        nonlocal key
+        key = akey
+
+    yield from\
+        dispatcher.node.chord_engine.tasks.send_store_data(\
+            content.encode(), store_key=True, key_callback=key_callback)
+
+    target_addr = fia(dd["target_addr"])
+
+    if not target_addr:
+        resp =\
+            "Resulting&nbsp;<a href='morphis://.dds/axon/read/{axon_addr}'>"\
+                "Axon</a>&nbsp;Address:<br/>{axon_addr}"\
+                     .format(axon_addr=mbase32.encode(key))
+
+        dispatcher.send_content(resp)
+        return
+
+    target_addr = mbase32.decode(target_addr)
+
+    synapse = tb.Synapse.for_target(target_addr, key)
+
+    yield from\
+        dispatcher.node.chord_engine.tasks.send_store_synapse(\
+            synapse, store_key=True, key_callback=key_callback)
+
+    resp =\
+        "Resulting&nbsp;<a href='morphis://.dds/axon/read/{synapse_addr}/"\
+            "{target_addr}'>Synapse</a>&nbsp;Address:<br/>{synapse_addr}"\
+                 .format(\
+                    synapse_addr=mbase32.encode(key),\
+                    target_addr=mbase32.encode(target_addr))
+
+    dispatcher.send_content(resp)
 
 @asyncio.coroutine
 def _format_axon(node, data, key, key_enc):
@@ -362,20 +382,6 @@ def __format_post(data):
         "<pre style='color: gray; padding:0;margin:0;'>{}</pre>"\
             .format(\
                 data[:end].decode(), make_safe_for_html_content(data[start:]))
-
-@asyncio.coroutine
-def _process_create_synapse(dispatcher, target_addr):
-    if dispatcher.handle_cache(target_addr):
-        return
-
-    template = templates.dds_create_synapse[0]
-
-    template = template.format(\
-        csrf_token=dispatcher.client_engine.csrf_token,\
-        message_text="",\
-        target_addr=target_addr)
-
-    dispatcher.send_content(template)
 
 @asyncio.coroutine
 def _load_axon(dispatcher, key, target_key):
