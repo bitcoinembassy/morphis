c63e66e3c098065e6dad0c71008cc26f65578d87
Sam Maloney <samm@morph.is>
2016-07-13 00:16:54 -0400

    Fix up hashbench.py.

--- hashbench.py
+++ hashbench.py
@@ -2,24 +2,28 @@
 # Written by Yu-Jie Lin
 # Public Domain
 
-from __future__ import print_function
+import llog
+
 import hashlib
 import os
 import sys
 import timeit
+import logging
+
+log = logging.getLogger(__name__)
 
 # https://pypi.python.org/pypi/pysha3/
 # sha3_* are introduced in Python 3.4+
 if sys.version_info < (3, 4):
-  import sha3
+    import sha3
 
 DATASIZE = 2**20
 REPEAT = 3
 NUMBER = 10
 HASHES = (
-  'md5',
-  'sha1', 'sha224', 'sha256', 'sha384', 'sha512',
-  'sha3_224', 'sha3_256', 'sha3_384', 'sha3_512',
+    'md5',
+    'sha1', 'sha224', 'sha256', 'sha384', 'sha512',
+#    'sha3_224', 'sha3_256', 'sha3_384', 'sha3_512',
 )
 
 width = max(len(f) for f in HASHES)
@@ -34,15 +38,20 @@ print('timeit for %d repeats, %d runs' % (REPEAT, NUMBER))
 print()
 
 for f in HASHES:
-  t = timeit.Timer(
-    '%s(data).hexdigest()' % f,
-    'from __main__ import data; from hashlib import %s' % f
-  )
-  result = t.repeat(repeat=REPEAT, number=NUMBER)
-  average = sum(result) / len(result)
-  print('{:{width}s}: {:9.6f} seconds @ {:9.6f} MiB/s'.format(
-    f,
-    average,
-    DATASIZE / average / (2**20),
-    width=width
-  ))
+    try:
+        t = timeit.Timer(
+            '%s(data).hexdigest()' % f,
+            'from __main__ import data; from hashlib import %s' % f
+        )
+        result = t.repeat(repeat=REPEAT, number=NUMBER)
+    except:
+        log.exception("Hash [{}]] failed with exception.".format(f))
+        continue
+
+    average = sum(result) / len(result)
+    print('{:{width}s}: {:9.6f} seconds @ {:9.6f} MiB/s'.format(
+        f,
+        average,
+        DATASIZE / average / (2**20),
+        width=width
+    ))
