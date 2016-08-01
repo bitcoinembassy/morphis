629dfcaaf7b5dd5428f9ed99edbc77327ee820be
Sam Maloney <samm@morph.is>
2016-08-01 06:42:50 -0400

    First draft implement autoscan of dds queries in clientengine/dds.

--- clientengine/dds.py
+++ clientengine/dds.py
@@ -19,6 +19,19 @@ import sshtype
 
 log = logging.getLogger(__name__)
 
+class DdsQuery(object):
+    def __init__(self, target_key):
+        self.target_key = target_key
+
+    def __hash__(self):
+        return has(self.target_key)
+
+    def __eq__(self, other):
+        return self.target_key == other.target_key
+
+    def __ne__(self, other):
+        return not(self == other)
+
 class DdsClientEngine(object):
     def __init__(self, node):
         self.node = node
@@ -27,11 +40,7 @@ class DdsClientEngine(object):
 
         self.dds_engine = dds.DdsEngine(node)
 
-        self.auto_scan_enabled = True
-
         self._running = False
-
-        self.autoscan_process = None
 
     @asyncio.coroutine
     def start(self):
@@ -40,28 +49,53 @@ class DdsClientEngine(object):
 
         self._running = True
 
-        if self.auto_scan_enabled:
-            self.autoscan_process = DdsAutoscanProcess(self)
-            self.autoscan_process.start()
-
     @asyncio.coroutine
     def stop(self):
         if not self._running:
             return
 
-        if self._autoscan_task:
-            self._autoscan_task.cancel()
+    def add_query_listener(self, listener, query):
+        assert type(query) is DdsQuery
+
+        process = self.enable_query_autoscan(query, False)
+        process.add_listener(listener)
+
+    def check_query_autoscan(self, query):
+        return self.autoscan_processes.get(query) is not None
+
+    def enable_query_autoscan(self, query, persistent=True):
+        assert type(query) is DdsQuery
+
+        process = self.autoscan_processes.get(query)
+        if process:
+            process.persistent = True
+            return process
+
+        process = DdsAutoscanProcess(self, query, persistent)
+        process.start()
+
+        self.autoscan_processes[query] = process
+
+        return process
 
 class DdsAutoscanProcess(object):
-    def __init__(self, dce):
+    def __init__(self, dce, query, persistent):
         self.dce = dce
         self.loop = dce.loop
+        self.query = query
+        self.persistent = persistent
 
         self._task = None
         self._running = False
 
+        self._listeners = []
+
     def start(self):
         assert not self._running
+        if log.isEnabledFor(logging.INFO):
+            log.info("Starting autoscan for target_key=[{}]."\
+                .format(self.query.target_key))
+
         self._running = True
         self._task = asyncio.async(self._run(), loop=self.loop)
 
@@ -73,7 +107,19 @@ class DdsAutoscanProcess(object):
         self._task.cancel()
         self._task = None
 
+    def add_listener(self, listener):
+        _listeners.append(listener)
+
+    def remove_listener(self, listener):
+        _listeners.remove(listener)
+
+    def _notify_listeners(self, post):
+        for listener in listeners:
+            listener(post)
+
     def _run(self):
         while self._running:
-            #TODO: YOU_ARE_HERE
-            pass
+            yield from self.dce.dds_engine.scan_target_key(\
+                self.query.target_key, self._notify_listeners)
+            # Let listeners know that the scan finished.
+            self._notify_listeners(None)
