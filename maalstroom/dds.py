f83b5b43285bec33a2a04a7c6c245b3fbfda720c
Sam Maloney <samm@morph.is>
2016-08-19 13:33:05 -0400

    Enhance Maalstroom Grok UI to show self posts first in list.

--- maalstroom/dds.py
+++ maalstroom/dds.py
@@ -11,7 +11,7 @@ import logging
 from urllib.parse import parse_qs, quote_plus
 import os
 
-from sqlalchemy import or_
+from sqlalchemy import or_, and_, desc
 
 import consts
 from db import User, DdsPost, NodeState
@@ -447,7 +447,8 @@ def _process_axon_synapses(req):
         with req.dispatcher.node.db.open_session(True) as sess:
             q = sess.query(DdsPost)\
                 .filter(DdsPost.target_key == axon_addr)\
-                .order_by(DdsPost.timestamp)
+                .order_by(desc(and_(DdsPost.signing_key != None, DdsPost.signing_key == DdsPost.target_key)),\
+                    DdsPost.timestamp)
 
             return q.all()
 
